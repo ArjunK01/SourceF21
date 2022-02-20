615e46fc9792b8be3bfe1fe302c5354906910e85
Arjun Kumar <arjunkumar01@gmail.com>
2022-02-20 16:45:42 -0500

    Update main.js

--- week2/main.js
+++ week2/main.js
@@ -1,132 +1,78 @@
-//variables
-let name = "arjun";
+let name = "Arjun";
+let age = 21;
 
-name = "vinay";
-name = true;
+name = "AG";
+name = "Lewi";
 
-//constants
+const class_name = "source";
 
-const age = 20;
+let over21 = age !== 21 ? "hi" : 12312;
 
-//function
-function add(a, b) {
-  let addition = a + b;
-  return addition;
+function add(num1, num2) {
+  return num1 + num2;
 }
 
-console.log(add(1, 2));
-
-//Arrow Function
-
-const subtract = (a, b) => {
-  return a - b;
+let subtract = function(num1, num2) {
+  return num1 - num2;
 };
-subtract(10, 7);
 
-const getTime = today => {
-  return today + " 5pm";
-  console.log("will never be run");
+let multiply = (num1, num2) => {
+  return num1 * num2;
 };
-console.log(getTime("Friday"));
 
-// function getTime(today){
-//     return today + " 5pm"
-// }
-
-const fullName = (firstName, lastName) => firstName + lastName;
-
-const multiply = (a, b) => {
-  return a * b;
+let divide = (num1, num2) => {
+  let result = num1 / num2;
+  return result;
 };
-//comment
 
-//conditional
-let num = 200;
-if (num < 10) {
-  console.log("num is less than 10");
-} else if (num < 100) {
-  console.log("num is less than 100");
-} else {
-  console.log("num is greater than 100");
-}
+let remainder = (num1, num2) => num1 % num2;
 
-let myAge = 20;
-if (myAge === 20) {
-  console.log("IM 20!");
-}
+console.log(remainder(10, 4));
 
-if (myAge < 100 && myAge > 0 && myAge === 19) {
-  console.log("age is less than 100");
-}
+console.log(multiply(1, 4));
+console.log(divide(42, 2));
 
-if (myAge < 0 || myAge === 20) {
-  console.log("here");
-}
+console.log(subtract(10, 5));
 
-//ternary
-let weather = "sunny";
+console.log(add(5, 10));
 
-let jacket = weather === "raining" ? true : false;
-
-console.log("I should wear a jacket: ", jacket);
-
-//loops
-
-//object
-let person = { name: "arjun", age: 20 };
-console.log(person.age);
-
-person.class = "source";
-
-console.log(person);
-
-//array
-let arrayOfNames = ["arjun", "vinay", "peter"];
-console.log(arrayOfNames[2]);
-
-arrayOfNames.push("sam");
-
-console.log(arrayOfNames);
-
-arrayOfNames.pop();
-
-console.log(arrayOfNames);
-
-arrayOfNames.unshift("McKayla");
-
-console.log(arrayOfNames);
-
-arrayOfNames.shift();
-console.log(arrayOfNames);
-
-console.log(arrayOfNames.length);
-
-console.log(arrayOfNames[arrayOfNames.length - 1]);
-
-let newNames = ["eddie", "cameron", "anna grace"];
-
-for (let i = 0; i < newNames.length; i++) {
-  console.log(newNames[i]);
-}
-
-newNames.forEach(temp => console.log(temp));
-
-//array of odd numbers 0-100, then add them all together and print them out
-
-//create an array of book objects (title, author),
-//then use a forEach to print author of
-//them each out
-
-//create an arrow function for converting Celsius to farenheit
-
-//Create an array of names, and create a function to delete a name based on
-//the parameter
-//ex. delete("arjun") should delete "arjun" from the array
-
-const App = () => {
-  return (
-    <div>
-      <p>Arjun!!!</p>
-    </div>
-  );
+let person = {
+  name: "Arjun",
+  age: 21
 };
+
+person.name = "Lewi";
+person.year = 4;
+
+console.log("Name:" + person.name);
+console.log("Age: " + person.age);
+console.log("Year: " + person.year);
+
+let classes = [
+  "Source Pro",
+  "Source Lite",
+  123123,
+  { name: "arjun" },
+  () => console.log("HI")
+];
+classes.push("Source");
+classes.push("Node");
+classes.push("Wireframe");
+
+console.log(classes);
+
+classes.pop();
+classes.pop();
+
+console.log(classes);
+
+classes.unshift("new first item");
+console.log(classes);
+classes.shift();
+console.log(classes);
+
+for (let i = 0; i < classes.length; i++) {
+  console.log(classes[i]);
+}
+
+classes.forEach((element) => console.log(element));
